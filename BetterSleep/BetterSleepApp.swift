c6d985fcfe8cd349bfd3001c2ca83ff53720f298
dnstrach <dnstrach@gmail.com>
2023-08-15 12:42:19 -0700

    date, stepper, CoreML

---
+++ BetterSleep/BetterSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleepApp.swift
+//  BetterSleep
+//
+//  Created by Dominique Strachan on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
