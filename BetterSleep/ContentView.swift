328ff33b96c48e135293035940939c11da865562
dnstrach <dnstrach@gmail.com>
2023-08-15 17:26:12 -0700

    dark mode, calculcated sleep displayed as section instead of alert, coffee cups inputed by picker

--- BetterSleep/ContentView.swift
+++ BetterSleep/ContentView.swift
@@ -11,7 +11,10 @@ import CoreML
 struct ContentView: View {
     @State private var wakeup = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    
+    @State private var selectedCoffeeAmount = 1
+    //@State private var coffeeAmount = 1
+    let coffeeAmount = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -27,43 +30,63 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
+                VStack(alignment: .leading, spacing: 10) {
                     Text("When do you want to wake up?")
                         .font(.headline)
                     
                     DatePicker("Please enter a time", selection: $wakeup, displayedComponents: .hourAndMinute).labelsHidden()
                 }
                 
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Desired amount of sleep:")
                         .font(.headline)
                     
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
                 
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
+//                    Text("Daily coffee intake")
+//                        .font(.headline)
                     
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Coffee amount:", selection: $selectedCoffeeAmount) {
+                        ForEach(coffeeAmount, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .font(.headline)
+                    
+                    Text(selectedCoffeeAmount == 1 ? "\(selectedCoffeeAmount) cup" : "\(selectedCoffeeAmount) cups")
+                    
+            
+                    
+//                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
                     
                 }
-            }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") {
-                    
+                
+                Section {
+                    Text(calculateBedtime())
+                        .font(.largeTitle)
+                        .foregroundColor(.orange)
+                }header: {
+                    Text("Your recommended bedtime is...")
                 }
-            } message: {
-                Text(alertMessage)
+                
             }
+            .navigationTitle("BetterSleep")
+//            .toolbar {
+//                Button("Calculate", action: calculateBedtime)
+//            }
+//            .alert(alertTitle, isPresented: $showingAlert) {
+//                Button("OK") {
+//
+//                }
+//            } message: {
+//                Text(alertMessage)
+//            }
         }
     }
     
-    func calculateBedtime() {
+    func calculateBedtime() -> String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -72,18 +95,41 @@ struct ContentView: View {
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(selectedCoffeeAmount))
             let sleepTime = wakeup - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            return formatter.string(from: sleepTime)
+            
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Sorry, there was an issue calculating your bedtime."
         }
-        
-        showingAlert = true
     }
+    
+    //USED FOR CALCULATE BUTTON IN NAV BAR ---> ALERT MESSAGE
+//    func calculateBedtime() {
+//        do {
+//            let config = MLModelConfiguration()
+//            let model = try SleepCalculator(configuration: config)
+//
+//            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeup)
+//            let hour = (components.hour ?? 0) * 60 * 60
+//            let minute = (components.minute ?? 0) * 60
+//
+//            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(selectedCoffeeAmount))
+//
+//            let sleepTime = wakeup - prediction.actualSleep
+//            alertTitle = "Your ideal bedtime is..."
+//            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+//        } catch {
+//            alertTitle = "Error"
+//            alertMessage = "Sorry, there was a problem calculating your bedtime."
+//        }
+//
+//        showingAlert = true
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
